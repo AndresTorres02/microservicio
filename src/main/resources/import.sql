5fd94f8b9b4d5db3ffb0fa2e6f35510cf34ee64d
Andres Torres <andrestorres@denkitronik.com>
2023-03-24 11:20:07 -0500

    Conexion de la base de datos pgadmin y intellij Se agrego los metodos GET, POST, PUT y DELETE

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,7 @@
 INSERT INTO pais (nombre) VALUES ('Colombia');
+INSERT INTO pais (nombre) VALUES ('Ecuador');
+INSERT INTO pais (nombre) VALUES ('Brasil');
+INSERT INTO pais (nombre) VALUES ('Argentina');
+INSERT INTO pais (nombre) VALUES ('Rusia');
+INSERT INTO pais (nombre) VALUES ('EEUU');
+INSERT INTO pais (nombre) VALUES ('Canada');
